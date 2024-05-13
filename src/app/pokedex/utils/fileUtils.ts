b151b777acfdbf421f7d5fec86f3ff5ac56e8a8b
Johannes Berger <johannes@berger.ac>
2024-05-13 11:43:21 +0200

    Update data for endpoints + file utils to start from root directory

--- src/app/pokedex/utils/fileUtils.ts
+++ src/app/pokedex/utils/fileUtils.ts
@@ -1,13 +1,12 @@
 import fsPromises from "fs/promises";
 import path from "path";
 
+export const rootDir = process.cwd();
+
 export async function getFile(folder: string, file: string) {
   const filePath = file.endsWith(".json") ? file : `${file}.json`;
   const jsonFile = await fsPromises.readFile(
-    path.resolve(
-      __dirname,
-      `../../../../public/data/pokedex/${folder}/${filePath}`
-    )
+    path.resolve(rootDir, `public/data/pokedex/${folder}/${filePath}`)
   );
   return JSON.parse(jsonFile.toString());
 }
@@ -16,7 +15,7 @@ export async function getFirstFileNameInFolder(
   folder: string
 ): Promise<string> {
   const files = await fsPromises.readdir(
-    path.resolve(__dirname, `../../../../public/data/pokedex/${folder}`)
+    path.resolve(rootDir, `public/data/pokedex/${folder}`)
   );
   return files[0];
 }
