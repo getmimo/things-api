7d494b0448c1b0ed708103ccd9d3817b9ce4c6a1
Johannes Berger <johannes@berger.ac>
2024-08-08 17:24:21 +0200

    Encode character to avoid build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-2 md:p-24 bg-gradient-to-r from-blue-500 to-purple-600 text-white">
       <div className="text-center mb-12">
         <h1 className="text-4xl font-bold mb-4">
-          Welcome to Mimo's API Playground
+          Welcome to Mimo&rsquo;s API Playground
         </h1>
         <p className="text-xl p-12">
           Explore the amazing world of RESTful APIs with Mimo
